a490be0678f076ea45516868b6fc7e2b7b24d706
Adrián Gaitán <agaitan@reachmyleads.com>
2024-03-03 15:09:34 -0500

    fix(home)

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,17 +5,11 @@ import { configureStore, applyMiddleware } from "@reduxjs/toolkit";
 import { onRepeatApi } from "./components/onRepeat";
 import { projectsApi } from "./components/projects";
 
-import thunkMiddleware from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
-
-const composedEnhancer = composeWithDevTools(applyMiddleware().apply(thunkMiddleware))
-
 export const store = configureStore({
   reducer: {
       [onRepeatApi.reducerPath]: onRepeatApi.reducer,
       [projectsApi.reducerPath]: projectsApi.reducer,
   },
-  composedEnhancer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
