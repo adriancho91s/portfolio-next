b5a8d8b68afd8296a2c0939665d26d63ddc5c6bd
Adrián Gaitán <adrian.gaitan@utp.edu.co>
2024-04-20 23:44:05 -0500

    feat(shorted): link workshop

--- src/app/[locale]/treeWorkshop/page.tsx
+++ src/app/[locale]/treeWorkshop/page.tsx
@@ -1,41 +1,26 @@
-import React from 'react';
-import {Metadata} from "next";
+import { redirect } from 'next/navigation'
 import Link from "next/link";
+import React from "react";
 
 const shortenedLink = 'https://drive.google.com/drive/folders/1-X2jDBnjREeiWcCLza69iEZLuGM_43S7';
 
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    metadataBase: new URL(shortenedLink),
-    title: `Arboles prueba escritorio, segunda parte | Adrian Gaitan`,
-    robots: {
-      index: true,
-      follow: true,
-      nocache: true,
-      googleBot: {
-        index: true,
-        follow: true,
-        noimageindex: true,
-        "max-video-preview": -1,
-        "max-image-preview": "large",
-        "max-snippet": -1,
-      },
-    },
-  };
-}
+const RedirectPage = async () => {
 
-const ShortenedLinkPage = () => {
+redirect(shortenedLink);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-      <h1>Arboles Prueba Escritorio, segunda prueba</h1>
-      <Link href={shortenedLink}>Link: https://drive.google.com/drive/folders/1-X2jDBnjREeiWcCLza69iEZLuGM_43S7</Link>
-      <p>Type: Google Drive Folder</p>
-      <p>Owner: Adrián Gaitán</p>
-    </div>
-    </main>
-  );
+        <h1>Arboles Prueba Escritorio, segunda prueba</h1>
+        <Link href={shortenedLink}>Link: https://drive.google.com/drive/folders/1-X2jDBnjREeiWcCLza69iEZLuGM_43S7</Link>
+        <p>Type: Google Drive Folder</p>
+        <p>Owner: Adrián Gaitán</p>
+      </div>
+      <div>
+        Redirecting
+        ...
+      </div>
+    </main>)
 };
 
-export default ShortenedLinkPage;
+export default RedirectPage;
